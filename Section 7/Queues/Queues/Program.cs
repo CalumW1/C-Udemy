4f823c0c67385a8be5bf58ad6a353326750fe1d3
Calum Wilson <wilsoncalum12@gmail.com>
2021-12-22 16:17:49 +1000

    Section 7: Stacks and Queues

---
+++ Section 7/Queues/Queues/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Queue<int> queue = new Queue<int>();
+
+            // add items to the queue;
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            // display the item at the front of the queue;
+            Console.WriteLine("Value at the start of the array {0} ", queue.Peek());
+
+            // remove items from the queue; 
+            queue.Dequeue();
+            Console.WriteLine("Value at the start of the array {0} ", queue.Peek());
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine("The value at the front of the queue is {0}", queue.Dequeue());
+
+                Console.WriteLine("Queue count is {0}", queue.Count);
+       
+            }*/
+
+            Queue<Order> ordersQueue = new Queue<Order>();
+
+            foreach (Order orders in RevievedOrdersFromBranch1())
+            {
+                ordersQueue.Enqueue(orders);
+            }
+
+            foreach (Order orders in RevievedOrdersFromBranch2())
+            {
+                ordersQueue.Enqueue(orders);
+            }
+
+            while (ordersQueue.Count > 0)
+            {
+                Order currentOrder = ordersQueue.Dequeue();
+                currentOrder.ProcessOrder();
+            }
+        }
+
+
+        static Order[] RevievedOrdersFromBranch1()
+        {
+            Order[] orders = new Order[] 
+            { 
+                new Order(3, 5),
+                new Order(4, 4),
+                new Order(5, 10)
+            };
+            return orders;
+        }
+
+        static Order[] RevievedOrdersFromBranch2()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(1, 5),
+                new Order(2, 4),
+                new Order(6, 10)
+            };
+            return orders;
+        }
+
+    }
+
+    class Order
+    {
+        public int OrderId { get; set; }
+
+        public int OrderQuantity { get; set; }
+
+        public Order(int id, int orderQuantity)
+        {
+            this.OrderId = id;
+            this.OrderQuantity = orderQuantity;
+        }
+
+        public void ProcessOrder()
+        {
+            Console.WriteLine("Order {0} processed!", OrderId);
+        }
+    }
+}
---
+++ Section 7/Stacks/Stacks/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            // add an element to the stack
+            stack.Push(1);
+
+            // Peek() will return the element at the top of the stack without removing it
+            Console.WriteLine("Top value in the stack is {0}", stack.Peek());
+
+            // push a bunch of items to the stack
+            stack.Push(2);
+            Console.WriteLine("Top value in the stack is {0}", stack.Peek());
+            stack.Push(3);
+            Console.WriteLine("Top value in the stack is {0}", stack.Peek());
+            stack.Push(4);
+            // remove from stack
+            /*int stackINT = stack.Pop();
+            Console.WriteLine("Pop Test {0}", stackINT);*/
+
+            // using while loop to remove from stack
+
+            while(stack.Count > 0)
+            {
+                int stackINT = stack.Pop();
+                Console.WriteLine("Pop Test {0}", stackINT);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            // return array in reverse order
+
+            int[] numbers = new int[] { 8, 2, 3, 4, 7, 6, 2 };
+
+            Stack<int> myStack = new Stack<int>();
+
+            //insert numbers from the array into the stack
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                myStack.Push(numbers[i]);
+            }
+
+            // return the stack in reverse order;
+            foreach(int number in myStack)
+            {
+                Console.WriteLine("{0}", number);
+            }
+        }
+    }
+}
